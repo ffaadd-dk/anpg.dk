4824c87571e21716f43d997dbbacb9b75c32d46a
Christian Panton <christian@panton.org>
2024-10-23 14:29:43 +0200

    fix updates without producing diff

--- download.sh
+++ download.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-wget --post-file=query.xml -O data.json http://overpass-api.de/api/interpreter
+wget --post-file=query.xml -O data-tmp.json http://overpass-api.de/api/interpreter
+cat data-tmp.json | jq '{elements: .elements, osm3s: {copyright: .osm3s.copyright}}' > data.json
+rm data-tmp.json
 
